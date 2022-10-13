5c9724a8acb8f550aa7cb5c02a51a462b448bb14
Israel Castro <israel.castro@gmail.com>
2022-10-13 18:14:39 -0500

    add dir_output

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import json
 import os
 
 FILE_SOURCE = './data_mini.json'
+DIR_OUTPUT = './content'
 
 with open(FILE_SOURCE, 'r') as content:
   posts = json.load(content)
@@ -9,9 +10,9 @@ with open(FILE_SOURCE, 'r') as content:
     html = post.get('html').replace("\\/", "/").encode().decode('unicode_escape')
 
     # Create folders with slug name
-    os.makedirs(f"./content/{post.get('slug')}")
+    os.makedirs(f"{DIR_OUTPUT}/{post.get('slug')}")
     
-    with open(f"./content/{post.get('slug')}/index.md", 'w') as file:
+    with open(f"{DIR_OUTPUT}/{post.get('slug')}/index.md", 'w') as file:
       file.write('---\n')
       file.write(f"title: {post.get('title')}\n")
       file.write(f"date: {post.get('created_at')}\n")
